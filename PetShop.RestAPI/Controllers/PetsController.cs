525a374a57b7a56ca7ed63f3a3d5b87b545d7119
Kacper Bujko <kacperbujko@hotmail.dk>
2020-09-01 23:11:38 +0200

    I dont know

--- PetShop.RestAPI/Controllers/PetsController.cs
+++ PetShop.RestAPI/Controllers/PetsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PetShop.Core.ApplicationService;
 using PetShop.Core.Entities;
+using PetShop.Core.Entities.Enums;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -23,35 +24,44 @@ namespace PetShop.RestAPI.Controllers
 
         // GET: api/<PetsController>
         [HttpGet]
-        public List<Pet> Get()
+        public ActionResult<List<Pet>> GetPets()
         {
             return _petService.GetPets();
         }
 
-        // GET api/<PetsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET: api/<PetsController>
+        [HttpGet("{sort}")]
+        public ActionResult<List<Pet>> SortPetsByPrice(string sort)
         {
-            return "value";
+            return _petService.SortPetsByPrice();
+        }
+
+        // GET api/<PetsController>/5
+        [HttpGet("{type}")]
+        public ActionResult<List<Pet>> SearchByType( PetType type)
+        {
+            return _petService.SearchByType(type);
         }
 
         // POST api/<PetsController>
         [HttpPost]
-        public Pet Post([FromBody] Pet pet)
+        public Pet AddPet([FromBody] Pet pet)
         {
             return _petService.AddPet(pet);
         }
 
         // PUT api/<PetsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public Pet EditPet(int id, [FromBody] Pet pet)
         {
+            return _petService.EditPet(id, pet);
         }
 
         // DELETE api/<PetsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public Pet Delete(int id)
         {
+            return _petService.DeletePet(id);
         }
     }
 }
