609e2fd5ff9342e5eadabff8fcadd7b85448ef4a
Kacper Bujko <kacperbujko@hotmail.dk>
2020-09-04 11:39:53 +0200

    Interfaces

---
+++ Core/HelperClasses/Interfaces/IPetSearcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.HelperClasses.Interfaces
+{
+    public interface IPetSearcher
+    {
+        
+    }
+}
